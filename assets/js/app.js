0f4c02e5a423aad3653e0abcfe7259d20bdd4a02
ndorrh <ndorrh@gmail.com>
2022-09-05 20:20:04 +0100

    Adjust event listener to the menu close icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,11 @@ const hambger = document.querySelector('.hambuger');
 const menuItem = document.querySelector('.menu')
 const closebutton = document.querySelector('.close')
 const logo = document.querySelector('.logo')
+var x = window.matchMedia("(max-width: 768px)")
 
 hambger.addEventListener('click', function () {
 
-    if (menuItem.style.display = 'none') {
+    if (menuItem.style.display = 'none' && x.matches) {
         menuItem.style.display = 'block'
         closebutton.style.display = 'block'
         hambger.style.display = 'none'
@@ -15,7 +16,7 @@ hambger.addEventListener('click', function () {
 
 closebutton.addEventListener('click', function () {
 
-    if (menuItem.style.display = 'block') {
+    if (menuItem.style.display = 'block' && x.matches) {
         menuItem.style.display = 'none'
         closebutton.style.display = 'none'
         hambger.style.display = 'block'
