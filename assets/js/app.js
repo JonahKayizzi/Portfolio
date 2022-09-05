cd1932e4a59fd618b04a0583dd91a454ab93db2f
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-09-06 02:34:08 +0300

    Fix html, css and javascript linters

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,30 +3,23 @@ const menu = document.querySelector('.menu');
 const closebutton = document.querySelector('.close');
 const logo = document.querySelector('.logo');
 
-hambger.addEventListener('click', function () {
-    if (menu.style.display = 'none') {
-        menu.style.display = 'block'
-        closebutton.style.display = 'block'
-        hambger.style.display = 'none'
-        logo.style.display = 'none'
-    }
-})
+hambger.addEventListener('click', () => {
+  menu.style.display = 'block';
+  closebutton.style.display = 'block';
+  hambger.style.display = 'none';
+  logo.style.display = 'none';
+});
 
-closebutton.addEventListener('click', function () {
-    if (menu.style.display = 'block') {
-        menu.style.display = 'none'
-        closebutton.style.display = 'none'
-        hambger.style.display = 'block'
-        logo.style.display = 'block'
-    }
-})
+closebutton.addEventListener('click', () => {
+  menu.style.display = 'none';
+  closebutton.style.display = 'none';
+  hambger.style.display = 'block';
+  logo.style.display = 'block';
+});
 
-menu.addEventListener('click', function () {
-    if (menu.style.display = 'block') {
-        menu.style.display = 'none'
-        closebutton.style.display = 'none'
-        hambger.style.display = 'block'
-        logo.style.display = 'block'
-    }
-})
-
+menu.addEventListener('click', () => {
+  menu.style.display = 'none';
+  closebutton.style.display = 'none';
+  hambger.style.display = 'block';
+  logo.style.display = 'block';
+});
