e6b8bad3b86de2b989b7b2a7688411f964341e05
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-09-06 03:57:54 +0300

    Update js to resolve linter errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,14 @@ const openMenu = () => {
   closebutton.style.display = 'block';
   hambger.style.display = 'none';
   logo.style.display = 'none';
-}
+};
 
 const closeMenu = () => {
   menu.style.display = 'none';
   closebutton.style.display = 'none';
   hambger.style.display = 'block';
   logo.style.display = 'block';
-}
+};
 
 hambger.addEventListener('click', openMenu);
 
