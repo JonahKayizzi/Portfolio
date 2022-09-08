124bc6760b31a4b8308d6675b5e3e9476927ad9a
ndorrh <ndorrh@gmail.com>
2022-09-08 21:35:58 +0100

    Decleare  variables to store form elements

---
+++ assets/js/form-validation.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('contact');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const error = document.getElementById('error');
+const errorEmail = document.getElementById('errorEmail');
+
