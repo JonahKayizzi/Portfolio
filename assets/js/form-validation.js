a5d1cb65acfe42ce280016188bb90ffb54636110
ndorrh <ndorrh@gmail.com>
2022-09-08 22:02:08 +0100

    Add event listerner to the email input field

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -5,3 +5,14 @@ const message = document.getElementById('message');
 const error = document.getElementById('error');
 const errorEmail = document.getElementById('errorEmail');
 
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+email.addEventListener('input', () => {
+    const isValid = emailRegExp.test(email.value) && email.value === email.value.toLowerCase();
+
+    if(isValid){
+        errorEmail.textContent ='';
+    }else{
+        errorEmail.textContent = 'Email cannot have capital letters';
+    }
+});
